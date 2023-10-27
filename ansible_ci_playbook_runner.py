7cf356d356ace03b91b2f0c1bfc461ed25f3a0dc
David Kurasov <kurasov.david@huawei-partners.com>
2023-10-27 22:31:09 +0300

    further development

--- ansible_ci_playbook_runner.py
+++ ansible_ci_playbook_runner.py
@@ -2,39 +2,58 @@ import os
 import subprocess
 import yaml
 
+from typing import Union
+from enum import Enum
+
 ##### Global Vars
 
 CONFIG_FILE = "playbooks_config.yml"
 
+
 #####
 
 ##### Classes
+
+class CommandType(Enum):
+    GALAXY = {
+        'command': 'ansible-galaxy',
+        'value_name': 'galaxy_cli_options',
+        'global_value_name': 'global_galaxy_cli_options',
+        'is_playbook': False
+    }
+    PLAYBOOK = {
+        'command': 'ansible-playbook',
+        'value_name': 'cli_options',
+        'global_value_name': 'global_cli_options',
+        'is_playbook': True
+    }
+
 
 class CliOption:
 
     name: str
     value: str | None
 
-    def __init__(self, cli_config: dict):
+    def __init__(self, cli_config: dict[str,Union[int, str, bool]]):
         self.name = cli_config['name']
         self.value = self.resolve_value(cli_config)
 
-    def resolve_value(self, value_config: dict) -> str:
-        if not value_config.get('value',0):
+    def resolve_value(self, value_config: dict[str,Union[int, str, bool]]) -> str:
+        if not value_config.get('value',False):
             return None
         unprocessed_value = value_config['value']
-        if value_config.get('value_is_env_var',0):
+        if value_config.get('value_is_env_var',False):
             assert type(unprocessed_value) is str, "Values stored as env vars can be strings only"
             return unprocessed_value if not value_config['value_is_env_var'] else self.resolve_env_type_value(unprocessed_value)
         if not type(unprocessed_value) is list:
             return unprocessed_value
         result = list()
         for element in unprocessed_value:
-            if type(element) is dict:
+            if isinstance(element, dict):
                 result.append(self.resolve_dict_value(element))
             else:
                 result.append(element)
-        if not value_config.get('separator',0):
+        if not value_config.get('separator',False):
             raise Exception("No separator is specified")
         return value_config['separator'].join(result)
 
@@ -43,12 +62,12 @@ class CliOption:
         assert env_var is not None, f"{value} env var doesn't exist!"
         return env_var
 
-    def resolve_dict_value(self, dict_value: dict) -> str:
-        result_key = dict_value['name']
-        if dict_value.get('value_is_env_var',0):
-            result_value = dict_value['value'] if not dict_value['value_is_env_var'] else self.resolve_env_type_value(dict_value['value'])
+    def resolve_dict_value(self, value: dict[str,Union[int, str, bool]]) -> str:
+        result_key = value['name']
+        if value.get('value_is_env_var',False):
+            result_value = value['value'] if not value['value_is_env_var'] else self.resolve_env_type_value(value['value'])
         else:
-            result_value = dict_value['value']
+            result_value = value['value']
         return "{}={}".format(result_key,result_value)
 
 
@@ -59,7 +78,7 @@ class Command:
     playbook: str | None
     cli_args: list
 
-    def __init__(self, command_name: str, cli_options: list, playbook=None):
+    def __init__(self, command_name: str, cli_options: list[CliOption], playbook=None):
         self.command_name = command_name
         if playbook is not None:
             self.is_galaxy = False
@@ -69,7 +88,7 @@ class Command:
             self.playbook = None
         self.cli_args = self.command_args(cli_options)
 
-    def command_args(self, cli_options: list):
+    def command_args(self, cli_options: list[CliOption]):
         args_list = [self.command_name]
         for cli_option in cli_options:
             if cli_option.value is not None:
@@ -100,39 +119,40 @@ def load_config() -> dict:
     return config
 
 
-def parse_global_cli_options(command_type: str,config: dict) -> list:
+def parse_global_cli_options(command_type: CommandType, config: dict[str, Union[int, str, bool, list, dict]]) -> list:
     global_options = list()
-    glob_opt_key = 'global_cli_options' if command_type != 'galaxy' else 'global_galaxy_cli_options'
-    if config.get(glob_opt_key,0) and len(config[glob_opt_key]) > 0:
+    glob_opt_key = command_type.value['global_value_name']
+    if config.get(glob_opt_key,False) and len(config[glob_opt_key]) > 0:
         for option in config[glob_opt_key]:
             global_options.append(CliOption(option))
     return global_options
 
 
-def execute_command(command_type: str, playbook_info: dict, config: dict) -> None:
-    cli_opt_key = 'cli_options' if command_type != 'galaxy' else 'galaxy_cli_options'
+def execute_command(command_type: CommandType,
+                    playbook_info: dict[str, Union[int, str, bool, list, dict]],
+                    config: dict[str, Union[int, str, bool, list, dict]]) -> None:
+    cli_opt_key = command_type.value['value_name']
     global_cli_opts = parse_global_cli_options(command_type,config)
     cli_opts = list()
     for cli_opt in playbook_info[cli_opt_key]:
         cli_opts.append(CliOption(cli_opt))
     unified_cli_opts = list(set(global_cli_opts).union(cli_opts))
-    if command_type != 'galaxy':
-        command = Command('ansible-playbook',unified_cli_opts,playbook_info['path'])
-    else:
-        command = Command('ansible-galaxy',unified_cli_opts)
+    playbook_path = playbook_info['path'] if command_type.value['is_playbook'] else None
+    command = Command(command_type.value['command'],unified_cli_opts,playbook_path)
     command.run_command()
 
 
-def process_playbook_data(playbook_info: dict, config: dict) -> None:
+def process_playbook_data(playbook_info: dict[str, Union[int, str, bool, list, dict]],
+                          config: dict[str, Union[int, str, bool, list, dict]]) -> None:
     if playbook_info['galaxy_deps_required']:
-        if (not playbook_info.get('galaxy_cli_options',0) or
+        if (not playbook_info.get('galaxy_cli_options',False) or
             len(playbook_info['galaxy_cli_options']) == 0):
             raise ValueError("galaxy_cli_options must be defined")
-        execute_command('galaxy',playbook_info,config)
-    if (not playbook_info.get('cli_options',0) or
+        execute_command(CommandType.GALAXY,playbook_info,config)
+    if (not playbook_info.get('cli_options',False) or
         len(playbook_info['cli_options']) == 0):
         raise ValueError("Playbook-level cli_options must be defined")
-    execute_command('playbook',playbook_info,config)
+    execute_command(CommandType.PLAYBOOK,playbook_info,config)
 
 
 def main() -> None:
